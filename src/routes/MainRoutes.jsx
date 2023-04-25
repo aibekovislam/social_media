34f969fd1b1bd84b2ec9b418e49cb83e27593347
FastandSteady <BaielAsanbekov19@gmail.com>
2023-04-25 20:37:58 +0600

    first

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import SignPage from "../pages/SignPage";
 
 function MainRoutes() {
   return <Routes>
     <Route>
         <Route path="/home" element={<HomePage/>}/>
+        <Route path="/home" element={<SignPage/>}/>
     </Route>
   </Routes>
 }
