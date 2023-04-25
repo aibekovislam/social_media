fdebb32a2556ae358fb1afb133da1eaf9539515f
bayaman <bayaman23m@mail.ru>
2023-04-25 20:26:05 +0600

    layote

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import MainLayote from "../layots/MainLayote";
 
 function MainRoutes() {
   return <Routes>
-    <Route>
+    <Route element={<MainLayote/>}>
         <Route path="/home" element={<HomePage/>}/>
     </Route>
   </Routes>
