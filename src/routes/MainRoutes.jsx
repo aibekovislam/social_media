8ed6ee46a825fd16e91d4d801e502e40bf04ed73
Dair020 <isakovdair@icloud.com>
2023-04-25 20:23:01 +0600

    routes

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import ArticleList from "../pages/ArticleList";
 
 function MainRoutes() {
-  return <Routes>
-    <Route>
-        <Route path="/home" element={<HomePage/>}/>
-    </Route>
-  </Routes>
+  return (
+    <Routes>
+      <Route>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/article" element={<ArticleList />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default MainRoutes;
