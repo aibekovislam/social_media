44df66dc948445deb440131c3a03579088e5b73e
bayaman <bayaman23m@mail.ru>
2023-04-29 23:05:10 +0600

    chat

---
+++ src/components/panel/Chat.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../panel/chat.css"
+
+function Chat() {
+  return (
+    <div class="card">
+      <div class="img"></div>
+      <div class="textBox">
+        <div class="textContent">
+          <p class="h1">Bayaman </p>
+          <span class="span">12 min ago</span>
+        </div>
+        <p class="p">я люблю тебя </p>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import "../static/homePage.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import Chat from "../components/panel/Chat";
+// import message from "../components/message/Message";
+import Message from "../components/message/Message";
+
 
 function HomePage() {
-  return <div>qwertyuiopolkjh</div>;
+  return (
+    <div className="main-block">
+      <div className="left-block">
+        <div className="top">
+          <div className="navbar">
+            <MenuIcon className="burger-icon" />
+            <input
+              type="text"
+              name="text"
+              class="input-navbar"
+              placeholder="Type..."
+            ></input>
+          </div>
+          <div className="groups">
+            <div className="groups__span">
+              <span>All</span>
+            </div>
+            <div className="groups__span">
+              <span>личные</span>
+            </div>
+            <div className="groups__span">
+              <span>группы</span>
+            </div>
+          </div>
+        </div>
+        <div className="chats" style={{ marginTop: "10px" }}>
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+          <Chat />
+        </div>
+      </div>
+      <div className="right-block">
+        <Message/>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
