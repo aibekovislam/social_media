4ad8e4cae2af7d8e18d3c2ba55842a8b182ae84c
bayaman <bayaman23m@mail.ru>
2023-05-03 22:03:38 +0600

    post

--- src/components/info/RightInfo.jsx
+++ src/components/info/RightInfo.jsx
@@ -4,13 +4,21 @@ import News from './News';
 
 function RightInfo() {
   return (
-    <div className="right-block-main">
-      <h5 style={{ padding: "10px 5px" }}>Добавьте в свою ленту</h5>
-      {/* <Chat  /> */}
-      <News />
-      <News />
-      <News />
-      <p className="btn-news">смотреть рекомендации</p>
+    <div>
+      <div className="right-block-main">
+        <h5 style={{ padding: "10px 5px" }}>Добавьте в свою ленту</h5>
+        {/* <Chat  /> */}
+        <News />
+        <News />
+        <News />
+        <p className="btn-news">смотреть рекомендации</p>
+      </div>
+      <div className="advertisement">
+        <img style={{borderRadius:"10px"}}
+          src="https://media.licdn.com/media/AAYQAgTPAAgAAQAAAAAAADVuOvKzTF-3RD6j-qFPqhubBQ.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
